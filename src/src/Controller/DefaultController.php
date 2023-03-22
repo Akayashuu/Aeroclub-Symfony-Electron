4bfb460cd3d10b64688e96a0dcc62497b2241545
joey <joeyhossaert59122@gmail.com>
2023-03-22 16:42:49 +0000

    Add template and function test

--- src/src/Controller/DefaultController.php
+++ src/src/Controller/DefaultController.php
@@ -14,10 +14,13 @@ class DefaultController extends AbstractController
     #[Route('/', name: 'app_login')]
     public function index(Request $request, EntityManagerInterface $em, AvionsRepository $repo): Response
     {
-
-        
         return $this->render('login/index.html.twig', []);
     }
 
-    
+    #[Route('/test', name: 'app_test')]
+    public function test(Request $request, EntityManagerInterface $em, AvionsRepository $repo): Response
+    {
+        return $this->render('test/accueil.html.twig', []);
+    }
+
 }
