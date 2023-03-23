7224eb5dd220b8bf0a5123b6f154ab37ce3690a3
Akayashuu <sauvageleo1@gmail.com>
2023-03-23 19:39:37 +0100

    Added form lib, started to add conf app part

--- src/src/Controller/ConnexionController.php
+++ src/src/Controller/ConnexionController.php
@@ -6,14 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\AvionsRepository;
-use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityManager;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\DBAL\Connection;
+use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
+
+
 
 class ConnexionController extends AbstractController
 {
     #[Route('/', name: 'app_connexion')]
-    public function index(): Response
+    public function index(Request $request, AvionsRepository $avrp): Response
     {
+
+
         return $this->render('connexion/index.html.twig', []);
     }
 }
