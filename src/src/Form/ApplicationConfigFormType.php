f49895e3beac8b20e1236d1f3351008fad9ff860
Akayashuu <sauvageleo1@gmail.com>
2023-03-24 10:50:42 +0000

    Added DatabaseLogic which contain isConnected static function Added CustomPasswordEncoder Added configTemplate + config form https://symfony.com/doc/current/configuration/secrets.html

--- src/src/Form/ApplicationConfigFormType.php
+++ src/src/Form/ApplicationConfigFormType.php
@@ -5,13 +5,20 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ApplicationConfigFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
+            ->add('adresseIp', TextType::class, ['required' => true])
+            ->add('utilisateur', TextType::class, ['required' => true])
+            ->add('motdepasse', TextType::class, ['required' => true])
+            ->add('secretKey', TextType::class, ['required' => true])
+            ->add('save', SubmitType::Class)
+
         ;
     }
 
--- src/src/Logic/DatabaseLogic.php
+++ src/src/Logic/DatabaseLogic.php
@@ -1,8 +1,24 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Logic;
 
+use App\Repository\AvionsRepository;
 
 class DatabaseLogic {
+
+    /**
+     * @date 24/03/2023 - 10h48
+     * @author Sauvage Léo
+     * @param AvionsRepository $avionsRepository
+     * @return bool
+     */
+    static public function isConnected(AvionsRepository $avionsRepository):bool {
+        $k = $avionsRepository->findAll();
+        if(is_array($k)) {
+            return true;
+        } 
+        return false;
+    }
     
 }
