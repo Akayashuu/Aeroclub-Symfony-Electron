212f25a9e8367bc4ea758834b0fbf21f418d58dc
Akayashuu <sauvageleo1@gmail.com>
2023-04-02 19:44:50 +0200

    Update connection now home page verify if connection is do

--- src/src/Controller/AccueilController.php
+++ src/src/Controller/AccueilController.php
@@ -2,15 +2,21 @@
 
 namespace App\Controller;
 
+use App\Security\CustomAuth;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccueilController extends AbstractController
 {
     #[Route('/accueil', name: 'app_accueil')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('accueil/index.html.twig', []);
+        if(CustomAuth::isConnected($request)) {
+            return $this->render('accueil/index.html.twig', []);
+        } else {
+            return $this->redirectToRoute('app_connexion');
+        }
     }
 }
