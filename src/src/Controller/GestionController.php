ef5f4d538589e45c640dc3892a6fae66d72fc404
joey <joeyhossaert59122@gmail.com>
2023-04-05 15:42:19 +0000

    ajout des pages pour modif instructeur

--- src/src/Controller/GestionController.php
+++ src/src/Controller/GestionController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Avions;
+use App\Entity\Instructeurs;
 use App\Form\InsertAvionsFormType;
+use App\Form\InsertInstrucType;
 use App\Repository\AvionsRepository;
+use App\Repository\InstructeursRepository;
 use App\Repository\PermissionsRepository;
 use App\Security\CustomAuth;
 use Doctrine\ORM\EntityManagerInterface;
@@ -92,6 +95,66 @@ class GestionController extends AbstractController
             $Avions = $avionsRepository->findOneBy(["numAvions" => $id]);
             if($Avions || $id != null) {
                 $entityManager->remove($Avions);
+                $entityManager->flush();
+                return new JsonResponse(array('success' => true));
+            } else {
+                return new JsonResponse(array('success' => false));
+            }
+        } else {
+            return new JsonResponse(array('success' => false));
+        }
+    }
+
+    #[Route('/gestion/insertInstruc', name: 'instruc_create')]
+    public function createInstruc(Request $request, InstructeursRepository $InstructeursRepository, PermissionsRepository $permissionsRepository, EntityManagerInterface $em): Response
+    {
+        if(CustomAuth::isConnected($request) && CustomAuth::isAdmin($request, $permissionsRepository)) {
+            $form = $this->createForm(InsertInstrucType::class);
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid())
+            {
+                $registration = $form->getData();
+                $em ->persist($registration);
+                $em->flush();
+                return $this->redirectToRoute('app_show_instruc');
+            }
+            return $this->render('gestion/insert_instruc.html.twig', [
+                'form' => $form->createView()
+            ]);
+        } else {
+            return $this->redirectToRoute('app_connexion');
+        }
+    }
+    
+    #[Route('/gestion/editInstructeurs/{id}', name: 'instructeur_edit')]
+    public function editInstructeurs(Request $request, InstructeursRepository $instructeursRepository, PermissionsRepository $permissionsRepository, EntityManagerInterface $em, $id): Response
+    {
+        if(CustomAuth::isConnected($request) && CustomAuth::isAdmin($request, $permissionsRepository)) {
+            $instructeurs = $instructeursRepository->findOneBy(["numInstructeurs" => $id]);
+            $form = $this->createForm(InsertInstrucType::class, $instructeurs);
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid())
+            {
+                $registration = $form->getData();
+                $em ->persist($registration);
+                $em->flush();
+                return $this->redirectToRoute('app_show_instructeurs');
+            }
+            return $this->render('gestion/edit_instructeurs.html.twig', [
+                'form' => $form->createView()
+            ]);
+        } else {
+            return $this->redirectToRoute('app_connexion');
+        }
+    }
+
+    #[Route('/gestion/deleteInstruc/{id}', name: 'instruc_delete')]
+    public function deleteInstruc(Request $request, InstructeursRepository $instructeursRepository, PermissionsRepository $permissionsRepository, EntityManagerInterface $entityManager, $id): Response
+    {
+        if(CustomAuth::isConnected($request) && CustomAuth::isAdmin($request, $permissionsRepository)) {
+            $Instructeurs = $instructeursRepository->findOneBy(["numInstructeurs" => $id]);
+            if($Instructeurs || $id != null) {
+                $entityManager->remove($Instructeurs);
                 $entityManager->flush();
                 return new JsonResponse(array('success' => true));
             } else {
